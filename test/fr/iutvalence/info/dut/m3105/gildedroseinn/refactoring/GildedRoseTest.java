c1f5455a66cbef2b56efb0457dcf3f34dbe72402
Axel CÉARD <axel.ceard@iut-valence.fr>
2015-11-05 09:36:51 +0100

    iter4 AgedBrieIncreasesInQualityAsOlderItGetsForPositiveSellin RED

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 public class GildedRoseTest
 {
 	
+	private static final String	AGED_BRIE	= "Aged Brie";
 	private static final int	DUMMY_SELL_IN	= 20;
 	private static final int	DUMMY_QUALITY	= 10;
 	private static final String	DUMMY_NAME	= "a";
@@ -82,5 +83,27 @@ public class GildedRoseTest
 				assertEquals(dummyItem.getQuality() < 0, false);
 			}
 		}
+	}
+	
+	@Test
+	public void AgedBrieIncreasesInQualityAsOlderItGetsForPositiveSellin()
+	{
+		for(Item dummyItem : dummyItemList)
+		{
+			dummyItem.setName(AGED_BRIE);
+		}
+
+		for(int day = 1; day < 6; day ++)
+		{
+			GildedRose.passADay(dummyItemList);
+			
+			for(Item dummyItem : dummyItemList)
+			{
+				assertEquals(DUMMY_QUALITY + day, dummyItem.getQuality());
+				assertEquals(AGED_BRIE, dummyItem.getName());
+				assertEquals(DUMMY_SELL_IN - day, dummyItem.getSellIn());
+			}
+		}
+
 	}
 }
