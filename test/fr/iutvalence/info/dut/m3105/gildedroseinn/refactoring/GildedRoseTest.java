3e213d0245f303c5ea5091cd200904dd12b1aaaf
Axel CÉARD <axel.ceard@iut-valence.fr>
2015-11-05 08:52:54 +0100

    iter1 itemListItemsQualityDecreaseByOneEachDayWith RED

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -2,14 +2,43 @@ package fr.iutvalence.info.dut.m3105.gildedroseinn.refactoring;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class GildedRoseTest
 {
-	@Test
-	public void aSimpleBeginningTest()
+	
+	private static final int	DUMMY_SELL_IN	= 20;
+	private static final int	DUMMY_QUALITY	= 10;
+	private static final String	DUMMY_NAME	= "a";
+	
+	ArrayList<Item> dummyItemList;
+
+	@Before
+	public void before()
 	{
-		assertEquals(true,false);
+		dummyItemList = new ArrayList<Item>(); 
+		dummyItemList.add(new Item(DUMMY_NAME, DUMMY_SELL_IN, DUMMY_QUALITY)); 
+		dummyItemList.add(new Item(DUMMY_NAME, DUMMY_SELL_IN, DUMMY_QUALITY)); 
+		dummyItemList.add(new Item(DUMMY_NAME, DUMMY_SELL_IN, DUMMY_QUALITY)); 
+		dummyItemList.add(new Item(DUMMY_NAME, DUMMY_SELL_IN, DUMMY_QUALITY)); 
+		dummyItemList.add(new Item(DUMMY_NAME, DUMMY_SELL_IN, DUMMY_QUALITY)); 
+
+	}
+	
+	@Test
+	public void itemListItemsQualityDecreaseByOneEachDayWith()
+	{
+		GildedRose.updateItems(dummyItemList);
+		
+		for(Item dummyItem : dummyItemList)
+		{
+			assertEquals(dummyItem.getQuality(), DUMMY_QUALITY - 1);
+			assertEquals(dummyItem.getName(), DUMMY_NAME);
+			assertEquals(dummyItem.getSellIn(), DUMMY_SELL_IN - 1);
+		}
 	}
 
 }
